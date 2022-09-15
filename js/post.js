89b201418788f6976db97641e01e56c658d44db7
Vivek-1012 <110529479+Vivek-1012@users.noreply.github.com>
2022-09-15 13:12:57 +0530

    doc
    more detailed features added

--- js/post.js
+++ js/post.js
@@ -3,5 +3,12 @@ let activityContainer = document.querySelector('.activity-container');
 
 heartIcon.addEventListener("click", () => {
     activityContainer.classList.toggle('hide');
-    
+    let src = heartIcon.src.split('-')[0];
+    if(image/heart.src.includes('nofill')){
+        heartIcon.src = `${src}-fill.png`;
+
+    }else{
+        heartIcon.src = `${src}-nofill.png`;
+    }
+
 })
